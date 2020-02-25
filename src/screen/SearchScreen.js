0375e36980f4324fd41dadf4bb9b077666976967
panuwat33586 <panuwat33586@gmail.com>
2020-02-25 20:58:31 +0700

    extracting hook logic to another file

--- src/screen/SearchScreen.js
+++ src/screen/SearchScreen.js
@@ -1,33 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import { View, StyleSheet, Text } from 'react-native'
 import SearchBar from '../components/SearchBar'
-import yelp from '../api/yelp'
+import useResults from '../hooks/useResults'
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('')
-    const [results, setResults] = useState([])
-
-    const searchApi=async()=>{
-        try{
-       const response=await yelp.get('/search',{
-           params:{
-               limit:50,
-               term,
-               location:'san jose'
-           }
-       }
-       );
-       setResults(response.data.businesses)
-    }catch(err){
-        console.log(err)
-    }
-    }
+    const [searchApi,results]=useResults()
     return (
         <View>
             <SearchBar 
             term={term} 
             onTermChange={setTerm} 
-            onTermSubmit={searchApi} />
+            onTermSubmit={()=>searchApi(term)} />
             <Text>{term}</Text>
             <Text>We have found {results.length} results</Text>
         </View>
